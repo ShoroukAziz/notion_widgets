a1e57416f92741c4efce1115e5122bee02d1cd44
Shorouk Abdelaziz <contact@shorouk.dev>
2022-12-06 17:37:29 -0700

    Add files via upload

--- map2/script.js
+++ map2/script.js
@@ -1,46 +1,143 @@
-"use strict";
-var worlddataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json";
-var width = document.getElementById("k1").width;
-var height = "300";
-console.log(height);
+/*
+  Example: https://github.com/tombatossals/angular-leaflet-directive/blob/master/examples/0601-mixed-geojson-events-example.html
+  Angular Leaflet Directive: https://github.com/tombatossals/angular-leaflet-directive
+  Leaflet: https://github.com/Leaflet/Leaflet
+*/
 
-var countries = ["AFG", "ALA", "ALB", "DZA", "ASM", "AND", "AGO", "AIA", "ATA", "ATG", "ARG", "ARM", "ABW", "AUS", "AUT", "AZE", "BHS", "BHR", "BGD", "BRB", "BLR", "BEL", "BLZ", "BEN", "BMU", "BTN", "BOL", "BES", "BIH", "BWA", "BVT", "BRA", "IOT", "VGB", "BRN", "BGR", "BFA", "BDI", "CPV", "KHM", "CMR", "CAN", "CYM", "CAF", "TCD", "CHL", "CHN", "HKG", "MAC", "CXR", "CCK", "COL", "COM", "COG", "COK", "CRI", "CIV", "HRV", "CUB", "CUW", "CYP", "CZE", "PRK", "COD", "DNK", "DJI", "DMA", "DOM", "ECU", "EGY", "SLV", "GNQ", "ERI", "EST", "SWZ", "ETH", "FLK", "FRO", "FJI", "FIN", "FRA", "GUF", "PYF", "ATF", "GAB", "GMB", "GEO", "DEU", "GHA", "GIB", "GRC", "GRL", "GRD", "GLP", "GUM", "GTM", "GGY", "GIN", "GNB", "GUY", "HTI", "HMD", "VAT", "HND", "HUN", "ISL", "IND", "IDN", "IRN", "IRQ", "IRL", "IMN", "ISR", "ITA", "JAM", "JPN", "JEY", "JOR", "KAZ", "KEN", "KIR", "KWT", "KGZ", "LAO", "LVA", "LBN", "LSO", "LBR", "LBY", "LIE", "LTU", "LUX", "MDG", "MWI", "MYS", "MDV", "MLI", "MLT", "MHL", "MTQ", "MRT", "MUS", "MYT", "MEX", "FSM", "MCO", "MNG", "MNE", "MSR", "MAR", "MOZ", "MMR", "NAM", "NRU", "NPL", "NLD", "NCL", "NZL", "NIC", "NER", "NGA", "NIU", "NFK", "MNP", "NOR", "OMN", "PAK", "PLW", "PAN", "PNG", "PRY", "PER", "PHL", "PCN", "POL", "PRT", "PRI", "QAT", "KOR", "MDA", "REU", "ROU", "RUS", "RWA", "BLM", "SHN", "KNA", "LCA", "MAF", "SPM", "VCT", "WSM", "SMR", "STP", "", "SAU", "SEN", "SRB", "SYC", "SLE", "SGP", "SXM", "SVK", "SVN", "SLB", "SOM", "ZAF", "SGS", "SSD", "ESP", "LKA", "PSE", "SDN", "SUR", "SJM", "SWE", "CHE", "SYR", "TJK", "THA", "MKD", "TLS", "TGO", "TKL", "TON", "TTO", "TUN", "TUR", "TKM", "TCA", "TUV", "UGA", "UKR", "ARE", "GBR", "TZA", "UMI", "USA", "VIR", "URY", "UZB", "VUT", "VEN", "VNM", "WLF", "ESH", "YEM", "ZMB", "ZWE"];
+// Ää Öö Üü ß
+// Kosovo: CS-KM, Somaliland, Northern Cyprus
 
-d3.json(worlddataUrl, function (datas) {
-    var bubblesA = [];
-    var maxr = countries.length;
-    var range = 0;
+// Title and icon only needed on CodePen
+document.title = 'German Interactive Map';
+var link = document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+link.href = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/93930/de-flag.ico';
+document.getElementsByTagName('head')[0].appendChild(link);
 
-    var color = d3.scale.linear(d3.interpolate).domain([0, maxr]).range(["rgb(100, 140, 110)", "rgb(170, 200, 170)"]);
-    var colorbuble = d3.scale.ordinal(d3.interpolateSpectral(11)).domain([0, maxr]).range(d3.schemeSpectral[11]);
-
-    var bubble_map = new Datamap({
-        element: document.getElementById('k1'),
-        scope: 'world',
-        height: height,
-        width: width,        
-        responsive: true,
-        geographyConfig: {
-            popupOnHover: true,
-            highlightOnHover: true,
-           
-        }, fills: {
-            defaultFill: '#6bfa9d'
-        } 
+angular.module("app", ["leaflet-directive"])
+.controller('GermanMapCtrl', ["$scope", "$http", function($scope, $http) {
+  $scope.showCenterCoords = false;
+  $scope.countriesGeoJson = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/93930/countries.geo.json?v=11";
+  $scope.countriesJson = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/93930/countries.json?v=11";
+  // Events: https://github.com/tombatossals/angular-leaflet-directive/blob/master/src/services/events/leafletGeoJsonEvents.js
+  $scope.$on("leafletDirectiveMap.geojsonMouseover", function(event, feature, leafletEvent) {
+    countryMouseover(feature, leafletEvent);
+  });
+  $scope.$on("leafletDirectiveMap.geojsonClick", function(event, featureSelected, leafletEvent) {
+    countryClick(featureSelected, leafletEvent);
+  });
+  var continentProperties = {
+    "150": {
+      name: 'Europe',
+      colors: [ '#F00', '#C33', '#900', '#F33', '#F66' ]
+    },
+    "019": {
+      name: 'America',
+      colors: [ '#069', '#366', '#036', '#39C', '#69C' ]
+    },
+    "002": {
+      name: 'Africa',
+      colors: [ '#0C0', '#393', '#090', '#3F3', '#6F6' ]
+    },
+    "142": {
+      name: 'Asia',
+      colors: [ '#FC0', '#C93', '#990', '#FC3', '#FC6' ]
+    },
+    "009": {
+      name: 'Oceania',
+      colors: [ '#C06', '#936', '#906', '#C39', '#C69' ]
+    }
+  };
+  angular.extend($scope, {
+    center: {
+      lat: 57.0168, // 40.8471
+      lng: 15.6445, // 14.0625
+      zoom: 2 // 2
+    },
+    legend: {
+      colors: [ '#F00', '#069', '#0C0', '#FC0', '#C06' ],
+      labels: [ 'Europe', 'America', 'Africa', 'Asia', 'Oceania' ]
+    },
+    defaults: {
+      // Unlabeled Map
+      // http://mc.bbbike.org/mc/?num=2&mt0=mapnik&mt1=mapquest-eu
+      tileLayer: "http://{s}.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png"
+    }
+  });
+  function selectCountry(country, event, color, fillColor) {
+    var layer = event.target;
+    layer.setStyle({
+      weight: 2,
+      color: color,
+      fillColor: fillColor
     });
-    const svg = d3.select(document.getElementById('k1'))
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-        ;
-    countries.forEach((element, i) => {
-
-        bubble_map.options.fills[element] = colorbuble(i);
-        var obj = {};
-        obj[element] = color(i);
-        bubble_map.updateChoropleth(obj);
+    layer.bringToFront();
+  }
+  // Click function, called from the Leaflet Map Events
+  function countryClick(country, event) {
+    selectCountry(country, event, '#444', '#fff');
+    $scope.selectedCountry = country;
+    $scope.selectedCountryData = $scope.countries[country.id];
+    console.log("click: ", country.properties.name);
+    console.log($scope.selectedCountryData);
+  }
+  // Mouse over function, called from the Leaflet Map Events
+  function countryMouseover(country, event) {
+    selectCountry(country, event, '#666', '#ddd');
+  }
+  // Get a country paint color from the continents array of colors
+  function getColor(country) {
+    if (!country || !country["region-code"]) {
+      return "#FFF";
+    }
+    var colors = continentProperties[country["region-code"]].colors;
+    var index = country["alpha-3"].charCodeAt(0) % colors.length;
+    return colors[index];
+  }
+  function style(country) {
+    return {
+      fillColor: getColor($scope.countries[country.id]),
+      weight: 1, // 2
+      opacity: 1,
+      color: 'white',
+      //dashArray: '0', // '3'
+      fillOpacity: 0.7
+    };
+  }
+  // Get the countries data from a JSON
+  //$http.get("json/all.json").success(function(data, status) {
+  $http.get($scope.countriesJson).success(function(data, status) {
+    // Put the countries on an associative array
+    $scope.countries = {};
+    for (var i = 0; i < data.length; i++) {
+      var country = data[i],
+          id = country['alpha-3'];
+      country['alpha-3'] = country['alpha-3'].replace('_', '');
+      country.flag = (function(name) {
+        if (name == null) { return ''; }
+        if (name.indexOf('_') != -1) { return name; }
+        return name.toLowerCase();
+      })(country['alpha-2']);
+      $scope.countries[id] = country;
+    }
+    // Get the countries geojson data from a JSON
+    //$http.get("json/countries.geo.json").success(function(data, status) {
+    $http.get($scope.countriesGeoJson).success(function(data, status) {
+      angular.extend($scope, {
+        geojson: {
+          data: data,
+          style: style,
+          resetStyleOnMouseout: true
+        },
+        selectedCountry: {},
+        selectedCountryData: {}
+      });
+      for (var item in data) {
+        if (!$scope.countries[data[item].id]) {
+          console.log(data[item]);
+        }
+      }
     });
-    //
-
-});
+  });
+}]);
